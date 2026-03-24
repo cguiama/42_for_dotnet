3f50f9cdcf6c10b8faea9fd754f69b3c6e65274e
Guilherme <45200933+cguiama@users.noreply.github.com>
2026-03-24 17:27:23 -0300

    exemplo

---
+++ QuestLogs/Lv00/Exemplos/Exemplo_Quest01.cs
@@ -0,0 +1,11 @@
+// Exemplo estrutural para o arquivo Quest01.cs
+namespace Lv00;
+
+public static class Quest01
+{
+    // A assinatura do motor exigida na Quest
+    public static void PrintChar(char c)
+    {
+        // Sua engenharia sob as regras da Forja entrará aqui.
+    }
+}
