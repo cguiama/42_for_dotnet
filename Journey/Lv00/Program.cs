ee6b5e227dbfee2ce9aefa12faeac7947b4bbce1
Guilherme <45200933+cguiama@users.noreply.github.com>
2026-03-24 17:22:48 -0300

    program

--- Journey/Lv00/Program.cs
+++ Journey/Lv00/Program.cs
@@ -4,6 +4,14 @@ public class Program
 {
     public static void Main()
     {
-        Lv00.Quest01.PrintChar('G');
+        // [LABORATÓRIO DE TESTES LOCAIS]
+        // No exato momento em que a agulha de leitura entra nesta chave '{', o sistema operacional 
+        // empilha o primeiro 'Frame' de execução na Stack da sua máquina.
+
+        // O Scouter ignora completamente a execução deste bloco. 
+        // Use esta área livre de auditoria para invocar os motores que você forjará nas próximas Quests.
+
+        // Exemplo de invocação de teste (descomente após forjar a Quest 01):
+        // Lv00.Quest01.PrintChar('G');
     }
 }
